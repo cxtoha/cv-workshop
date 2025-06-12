6e03ec9d94c9efa247bb1f4145077d1485798072
ahmeds99 <ahmed99@live.no>
2025-06-12 14:04:38 +0200

    add missing file

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+using backend.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Experience> Experiences { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<User>()
+                .HasMany(u => u.Experiences)
+                .WithOne(e => e.User)
+                .HasForeignKey(e => e.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.SeedUsers();
+            modelBuilder.SeedExperiences();
+        }
+    }
+}
